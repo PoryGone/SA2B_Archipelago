3db6c31e93b9d916bce288eb5b4aeb5f18fed5d0
PoryGone <porygoneplays@gmail.com>
2022-11-29 02:02:01 -0500

    Updated APCpp

--- SA2B_Archipelago/SA2B_Archipelago/Archipelago/ArchipelagoManager.cpp
+++ SA2B_Archipelago/SA2B_Archipelago/Archipelago/ArchipelagoManager.cpp
@@ -104,14 +104,14 @@ void ArchipelagoManager::OnFrameFunction()
 
     this->_authFailed = false;
 
-    this->_keepAliveTimer++;
-
-    if (this->_keepAliveTimer >= KEEP_ALIVE)
-    {
-        this->_keepAliveTimer = 0;
-
-        AP_KeepAlive();
-    }
+    //this->_keepAliveTimer++;
+    //
+    //if (this->_keepAliveTimer >= KEEP_ALIVE)
+    //{
+    //    this->_keepAliveTimer = 0;
+    //
+    //    AP_KeepAlive();
+    //}
 
     AP_RoomInfo RoomInfo;
     AP_GetRoomInfo(&RoomInfo);
@@ -186,14 +186,14 @@ void SA2_ResetItems()
     apm->ResetItems();
 }
 
-void SA2_RecvItem(int item_id, bool notify)
+void SA2_RecvItem(int64_t item_id, bool notify)
 {
     ArchipelagoManager* apm = &ArchipelagoManager::getInstance();
 
     apm->ReceiveItem(item_id, notify);
 }
 
-void SA2_CheckLocation(int loc_id)
+void SA2_CheckLocation(int64_t loc_id)
 {
     ArchipelagoManager* apm = &ArchipelagoManager::getInstance();
 
@@ -476,10 +476,63 @@ void ArchipelagoManager::OnFrameMessageQueue()
     }
 
     MessageQueue* messageQueue = &MessageQueue::GetInstance();
-    std::vector<std::string> msg = AP_GetLatestMessage();
-    for (unsigned int i = 0; i < msg.size(); i++)
+    //std::vector<std::string> msg = AP_GetLatestMessage();
+    //for (unsigned int i = 0; i < msg.size(); i++)
+    //{
+    //    messageQueue->AddMessage(msg.at(i));
+    //}
+    AP_Message* msg = AP_GetLatestMessage();
+    std::vector<std::string> outMsgs;
+    if (msg)
     {
-        messageQueue->AddMessage(msg.at(i));
+        switch (msg->type)
+        {
+        case (AP_MessageType::ItemSend):
+        {
+            AP_ItemSendMessage* sendMsg = static_cast<AP_ItemSendMessage*>(msg);
+
+            if (sendMsg)
+            {
+                std::string outMsg = "Sent " + sendMsg->item + " to " + sendMsg->recvPlayer;
+                outMsgs.push_back(outMsg);
+            }
+            break;
+        }
+        case (AP_MessageType::ItemRecv):
+        {
+            AP_ItemRecvMessage* recvMsg = static_cast<AP_ItemRecvMessage*>(msg);
+
+            if (recvMsg)
+            {
+                std::string outMsg = "Received " + recvMsg->item + " from " + recvMsg->sendPlayer;
+                outMsgs.push_back(outMsg);
+            }
+            break;
+        }
+        case (AP_MessageType::Hint):
+        {
+            AP_HintMessage* hintMsg = static_cast<AP_HintMessage*>(msg);
+
+            if (hintMsg)
+            {
+                std::string foundText = hintMsg->checked ? " (found)" : " (not found)";
+                std::string outMsg1 = hintMsg->recvPlayer + "'s " + hintMsg->item + " can be found at";
+                std::string outMsg2 = "  " + hintMsg->location + " in " + hintMsg->sendPlayer + "'s world." + foundText;
+                outMsgs.push_back(outMsg1);
+                outMsgs.push_back(outMsg2);
+            }
+            break;
+        }
+        default:
+        {
+            std::string outMsg = msg->text;
+        }
+        }
+
+        for (unsigned int i = 0; i < outMsgs.size(); i++)
+        {
+            messageQueue->AddMessage(outMsgs.at(i).c_str());
+        }
     }
     AP_ClearLatestMessage();
 }
--- SA2B_Archipelago/lib/APCpp/Archipelago.h
+++ SA2B_Archipelago/lib/APCpp/Archipelago.h
@@ -16,14 +16,97 @@ struct AP_NetworkVersion {
     int build;
 };
 
+// Set current client version
+void AP_SetClientVersion(AP_NetworkVersion*);
+
+/* Configuration Functions */
+
+void AP_EnableQueueItemRecvMsgs(bool);
+
+void AP_SetDeathLinkSupported(bool);
+
+/* Required Callback Functions */
+
+//Parameter Function must reset local state
+void AP_SetItemClearCallback(void (*f_itemclr)());
+//Parameter Function must collect item id given with parameter. Secound parameter indicates whether or not to notify player
+void AP_SetItemRecvCallback(void (*f_itemrecv)(int64_t,bool));
+//Parameter Function must mark given location id as checked
+void AP_SetLocationCheckedCallback(void (*f_locrecv)(int64_t));
+
+/* Optional Callback Functions */
+
+//Parameter Function will be called when Death Link is received. Alternative to Pending/Clear usage
+void AP_SetDeathLinkRecvCallback(void (*f_deathrecv)());
+
+// Parameter Function receives Slotdata of respective type
+void AP_RegisterSlotDataIntCallback(std::string, void (*f_slotdata)(int));
+void AP_RegisterSlotDataMapIntIntCallback(std::string, void (*f_slotdata)(std::map<int,int>));
+void AP_RegisterSlotDataRawCallback(std::string, void (*f_slotdata)(std::string));
+
+/* Game Management Functions */
+
+// Sends LocationCheck for given index
+void AP_SendItem(int64_t);
+
+// Called when Story completed, sends StatusUpdate
+void AP_StoryComplete();
+
+/* Deathlink Functions */
+
+bool AP_DeathLinkPending();
+void AP_DeathLinkClear();
+void AP_DeathLinkSend();
+
+/* Message Management Types */
+
+enum AP_MessageType {
+    Plaintext, ItemSend, ItemRecv, Hint, Countdown
+};
+
+struct AP_Message {
+    AP_MessageType type = AP_MessageType::Plaintext;
+    std::string text;
+};
+
+struct AP_ItemSendMessage : AP_Message {
+    std::string item;
+    std::string recvPlayer;
+};
+
+struct AP_ItemRecvMessage : AP_Message {
+    std::string item;
+    std::string sendPlayer;
+};
+
+struct AP_HintMessage : AP_Message {
+    std::string item;
+    std::string sendPlayer;
+    std::string recvPlayer;
+    std::string location;
+    bool checked;
+};
+
+struct AP_CountdownMessage : AP_Message {
+    int timer;
+};
+
+/* Message Management Functions */
+
+bool AP_IsMessagePending();
+void AP_ClearLatestMessage();
+AP_Message* AP_GetLatestMessage();
+
+/* Connection Information Types */
+
+enum AP_ConnectionStatus {
+    Disconnected, Connected, Authenticated, ConnectionRefused
+};
+
 #define AP_PERMISSION_DISABLED 0b000
 #define AP_PERMISSION_ENABLED 0b001
 #define AP_PERMISSION_GOAL 0b010
 #define AP_PERMISSION_AUTO 0b110
-
-enum AP_ConnectionStatus {
-    Disconnected, Connected, Authenticated
-};
 
 struct AP_RoomInfo {
     AP_NetworkVersion version;
@@ -40,38 +123,61 @@ struct AP_RoomInfo {
     float time;
 };
 
+/* Connection Information Functions */
+
 int AP_GetRoomInfo(AP_RoomInfo*);
-
-void AP_KeepAlive();
-
-void AP_EnableQueueItemRecvMsgs(bool);
-
-void AP_SetDeathLinkSupported(bool);
-
-//Parameter Function must reset local state
-void AP_SetItemClearCallback(void (*f_itemclr)());
-//Parameter Function must collect item id given with parameter. Secound parameter indicates whether or not to notify player
-void AP_SetItemRecvCallback(void (*f_itemrecv)(int,bool));
-//Parameter Function must mark given location id as checked
-void AP_SetLocationCheckedCallback(void (*f_locrecv)(int));
-//Parameter Function will be called when Death Link is received. Alternative to Pending/Clear usage
-void AP_SetDeathLinkRecvCallback(void (*f_deathrecv)());
-
-void AP_RegisterSlotDataIntCallback(std::string, void (*f_slotdata)(int));
-void AP_RegisterSlotDataMapIntIntCallback(std::string, void (*f_slotdata)(std::map<int,int>));
-
-// Sends LocationCheck for given index
-void AP_SendItem(int);
-
-bool AP_DeathLinkPending();
-void AP_DeathLinkClear();
-void AP_DeathLinkSend();
-
-// Called when Story completed, sends StatusUpdate
-void AP_StoryComplete();
-
-bool AP_IsMessagePending();
-void AP_ClearLatestMessage();
-std::vector<std::string> AP_GetLatestMessage();
-
 AP_ConnectionStatus AP_GetConnectionStatus();
+int AP_GetUUID();
+
+/* Serverside Data Types */
+
+enum AP_RequestStatus {
+    Pending, Done, Error
+};
+
+enum AP_DataType {
+    Raw, /*Int, Double*/
+};
+
+struct AP_GetServerDataRequest {
+    AP_RequestStatus status;
+    std::string key;
+    void* value;
+    AP_DataType type;
+};
+
+struct AP_DataStorageOperation {
+    std::string operation;
+    void* value;
+};
+
+struct AP_SetServerDataRequest {
+    AP_RequestStatus status;
+    std::string key;
+    std::vector<AP_DataStorageOperation> operations;
+    void* default_value;
+    AP_DataType type;
+    bool want_reply;
+};
+
+struct AP_SetReply {
+    std::string key;
+    void* original_value;
+    void* value;
+};
+
+/* Serverside Data Functions */
+
+// Set and Receive Data
+void AP_SetServerData(AP_SetServerDataRequest* request);
+void AP_GetServerData(AP_GetServerDataRequest* request);
+
+// Parameter Function receives all SetReply's
+// ! Pointers in AP_SetReply struct only valid within function !
+// If values are required beyond that a copy is needed
+void AP_RegisterSetReplyCallback(void (*f_setreply)(AP_SetReply));
+
+// Receive all SetReplys with Keys in parameter list
+void AP_SetNotify(std::map<std::string,AP_DataType>);
+// Single Key version of above for convenience
+void AP_SetNotify(std::string, AP_DataType);
