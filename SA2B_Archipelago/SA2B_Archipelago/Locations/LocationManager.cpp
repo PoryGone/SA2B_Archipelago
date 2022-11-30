317ba3b0458f854cdee12f9b8a218f27abace681
PoryGone <porygoneplays@gmail.com>
2022-11-29 23:32:20 -0500

    Always unlock Chao Garden

--- SA2B_Archipelago/SA2B_Archipelago/Locations/LocationManager.cpp
+++ SA2B_Archipelago/SA2B_Archipelago/Locations/LocationManager.cpp
@@ -331,6 +331,9 @@ void LocationManager::OnFrameGoldBeetles()
 
 void LocationManager::OnFrameChaoGarden()
 {
+	//Make All Characters have Chao Garden Access
+	WriteData<8>((void*)0x1DEF829, 0x01);
+
 	if (!this->_chaoEnabled)
 	{
 		// Don't do any Chao stuff if no Chao checks are on
@@ -339,9 +342,6 @@ void LocationManager::OnFrameChaoGarden()
 
 	// Make sure Hero/Dark Gardens are always unlocked
 	ChaoGardensUnlocked = 0x56;
-
-	//Make All Characters have Chao Garden Access
-	WriteData<8>((void*)0x1DEF829, 0x01);
 
 	// Handle Separate Chao Saves
 	std::string chaoFileName = ArchipelagoManager::getInstance().GetSeedName().substr(0, 11);
