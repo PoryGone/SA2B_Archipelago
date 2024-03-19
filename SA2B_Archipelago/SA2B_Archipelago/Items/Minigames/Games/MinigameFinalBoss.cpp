838435d77089e4f50223ac181e65da80e69a09c2
RaspberrySpaceJam <tyler.summers@gmail.com>
2024-03-18 22:56:02 -0700

    Progress on final

--- SA2B_Archipelago/SA2B_Archipelago/Items/Minigames/Games/MinigameFinalBoss.cpp
+++ SA2B_Archipelago/SA2B_Archipelago/Items/Minigames/Games/MinigameFinalBoss.cpp
@@ -8,7 +8,19 @@ void MinigameFinalBoss::OnGameStart(MinigameManagerData data)
 	state = FBS_Intro;
 	introState = FBIS_FadeIn;
 	sonicIsActive = true;
-	bossHealth = 100.0f;
+	bossHealth = bossMaxHealth;
+	sonicRingCount = 50;
+	shadowRingCount = 50;
+	data.timers->push_back(&characterShootTimer);
+	data.timers->push_back(&ringDecayTimer);
+	data.timers->push_back(&ringGrowthTimer);
+	data.timers->push_back(&weakPointTimer);
+	weakSpotIndex = 0;
+	std::shuffle(weakPointPositions.begin(), weakPointPositions.end(), RNG());
+	for (int i = 0; i < patternTimers.size(); i++)
+	{
+		data.timers->push_back(&patternTimers[i]);
+	}
 	CreateHierarchy(data);
 }
 
@@ -49,6 +61,9 @@ void MinigameFinalBoss::RunIntro(MinigameManagerData data)
 		shadow->SetPosition({ 0.0f, 0.0f });
 		shadow->SetEnabled(false);
 		FHParent->SetPositionGlobal({ 320.0f, 0.0f });
+		ringDecayTimer.Start(ringDecayCooldown);
+		ringGrowthTimer.Start(ringGrowthCooldown);
+		FHHealthBarBG->SetEnabled(true);
 		state = FBS_InGame;
 	}
 	switch (introState)
@@ -88,6 +103,9 @@ void MinigameFinalBoss::RunIntro(MinigameManagerData data)
 		{
 			sonic->SetPosition({ 0.0f, 0.0f });
 			shadow->SetEnabled(false);
+			ringDecayTimer.Start(ringDecayCooldown);
+			ringGrowthTimer.Start(ringGrowthCooldown);
+			FHHealthBarBG->SetEnabled(true);
 			state = FBS_InGame;
 		}
 		break;
@@ -97,6 +115,12 @@ void MinigameFinalBoss::RunIntro(MinigameManagerData data)
 void MinigameFinalBoss::RunInGame(MinigameManagerData data)
 {
 	UpdateCharacterPosition(data);
+	UpdateBullets(data);
+	UpdateWeakPoint();
+	if (data.input & RIF_A && characterShootTimer.IsElapsed())
+	{
+		CharacterShoot(data);
+	}
 	if (data.inputPress & RIF_B)
 	{
 		if (sonicIsActive)
@@ -110,10 +134,19 @@ void MinigameFinalBoss::RunInGame(MinigameManagerData data)
 		sonicIsActive = !sonicIsActive;
 		state = FBS_Swap;
 	}
+	UpdateCharacterRingGrowthDecay(data);
 }
 
 void MinigameFinalBoss::RunSwap(MinigameManagerData data)
 {
+	characterShootTimer.Pause();
+	ringDecayTimer.Pause();
+	ringGrowthTimer.Pause();
+	weakPointTimer.Pause();
+	for (int i = 0; i < patternTimers.size(); i++)
+	{
+		patternTimers[i].Pause();
+	}
 	sonic->SetEnabled(true);
 	shadow->SetEnabled(true);
 	NJS_POINT3 sonicToPos = sonicIsActive ? characterParent->GetPositionGlobal() : sonicOffScreenPos;
@@ -125,6 +158,72 @@ void MinigameFinalBoss::RunSwap(MinigameManagerData data)
 		sonic->SetEnabled(sonicIsActive);
 		shadow->SetEnabled(!sonicIsActive);
 		state = FBS_InGame;
+		characterShootTimer.Resume();
+		ringDecayTimer.Resume();
+		ringGrowthTimer.Resume();
+		for (int i = 0; i < patternTimers.size(); i++)
+		{
+			patternTimers[i].Resume();
+		}
+	}
+}
+
+void MinigameFinalBoss::CharacterShoot(MinigameManagerData data)
+{
+	for (int i = 0; i < characterBullets.size(); i++)
+	{
+		if (!characterBullets[i].node->IsEnabled())
+		{
+			characterBullets[i].node->SetEnabled(true);
+			characterBullets[i].node->SetPositionGlobal(characterParent->GetPositionGlobal());
+			characterBullets[i].velocity = Point3Scale({ 0.0f,-1.0f }, characterBulletSpeed);
+			characterShootTimer.Start(characterBulletCooldown);
+			return;
+		}
+	}
+}
+
+void MinigameFinalBoss::UpdateBullets(MinigameManagerData data)
+{
+	//Character Bullets
+	float yMin = 0.0f - (characterBulletSize.y * 0.5f) - 5.0f;
+	float yMax = 480.0f + (characterBulletSize.y * 0.5f) + 5.0f;
+	float xMin = 20.0f - (characterBulletSize.x * 0.5f) - 5.0f;
+	float xMax = 620.0f + (characterBulletSize.x * 0.5f) + 5.0f;
+	for (int i = 0; i < characterBullets.size(); i++)
+	{
+		if (characterBullets[i].node->IsEnabled())
+		{
+			characterBullets[i].node->Translate(characterBullets[i].velocity);
+			if (data.collision->IsColliding(FHWeakPoint, characterBullets[i].node))
+			{
+				bossHealth -= 1.0f;
+				characterBullets[i].node->SetEnabled(false);
+				UpdateHealthBarFill();
+			}
+			NJS_POINT3 pos = characterBullets[i].node->GetPositionGlobal();
+			if (pos.x < xMin || pos.x > xMax || pos.y < yMin || pos.y > yMax)
+			{
+				characterBullets[i].node->SetEnabled(false);
+			}
+		}
+	}
+	//Boss Bullets
+	yMin = 0.0f - (bossBulletSize.y * 0.5f) - 5.0f;
+	yMax = 480.0f + (bossBulletSize.y * 0.5f) + 5.0f;
+	xMin = 20.0f - (bossBulletSize.x * 0.5f) - 5.0f;
+	xMax = 620.0f + (bossBulletSize.x * 0.5f) + 5.0f;
+	for (int i = 0; i < bossBullets.size(); i++)
+	{
+		if (bossBullets[i].node->IsEnabled())
+		{
+			bossBullets[i].node->Translate(bossBullets[i].velocity);
+			NJS_POINT3 pos = bossBullets[i].node->GetPositionGlobal();
+			if (pos.x < xMin || pos.x > xMax || pos.y < yMin || pos.y > yMax)
+			{
+				bossBullets[i].node->SetEnabled(false);
+			}
+		}
 	}
 }
 
@@ -164,11 +263,83 @@ void MinigameFinalBoss::UpdateCharacterPosition(MinigameManagerData data)
 	characterParent->SetPositionGlobal(pos);
 }
 
+void MinigameFinalBoss::UpdateCharacterRingGrowthDecay(MinigameManagerData data)
+{
+	if (ringGrowthTimer.IsElapsed())
+	{
+		if (sonicIsActive)
+		{
+			shadowRingCount = shadowRingCount < 999 ? shadowRingCount + 1 : shadowRingCount;
+		}
+		else
+		{
+			sonicRingCount = sonicRingCount < 999 ? sonicRingCount + 1 : sonicRingCount;
+		}
+		ringGrowthTimer.Start(ringGrowthCooldown);
+	}
+	if (ringDecayTimer.IsElapsed())
+	{
+		if (sonicIsActive)
+		{
+			sonicRingCount = sonicRingCount > 0 ? sonicRingCount - 1 : sonicRingCount;
+		}
+		else
+		{
+			shadowRingCount = shadowRingCount > 0 ? shadowRingCount - 1 : shadowRingCount;
+		}
+		ringDecayTimer.Start(ringDecayCooldown);
+	}
+	std::string text = "Sonic\n";
+	text.append(std::to_string(sonicRingCount));
+	sonicRingsText->UpdateText(text);
+	text = "Shadow\n";
+	text.append(std::to_string(shadowRingCount));
+	shadowRingsText->UpdateText(text);
+}
+
+void MinigameFinalBoss::UpdateWeakPoint()
+{
+	if (weakPointTimer.IsElapsed())
+	{
+		weakSpotIndex++;
+		if (weakSpotIndex >= weakPointPositions.size())
+		{
+			weakSpotIndex = 0;
+		}
+		FHWeakPoint->SetPosition(weakPointPositions[weakSpotIndex]);
+		FHWeakPoint->SetEnabled(true);
+		weakPointTimer.Start(weakPointCooldown);
+	}
+}
+
+void MinigameFinalBoss::UpdateHealthBarFill()
+{
+	float amount = bossHealth / bossMaxHealth;
+	amount = amount < 0.0f ? 0.0f : amount;
+	amount = amount > 1.0f ? 1.0f : amount;
+	float width = FHHealthBarBG->displaySize.x * amount;
+	float bgX = -(FHHealthBarBG->displaySize.x * 0.5f);
+	NJS_POINT3 pos = { bgX + width * 0.5f, 0.0f};
+	FHHealthBar->displaySize.x = width;
+	FHHealthBar->SetPosition(pos);
+}
+
 void MinigameFinalBoss::CreateHierarchy(MinigameManagerData data)
 {
 	//Create Background
 	background = data.hierarchy->CreateNode("BG", data.icons->GetAnim(MGI_White_Box), { data.icons->xMax - data.icons->xMin + 2.0f, data.icons->yMax + 2.0f, 0.0f }, { data.icons->xCenter, data.icons->yCenter, 0.0f });
 	background->color = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+	//Create Boss Bullets
+	bossBulletParent = data.hierarchy->CreateNode("Boss_Bullets");
+	bossBullets.clear();
+	for (int i = 0; i < 50; i++)
+	{
+		MinigameBulletData bullet{};
+		bullet.node = data.hierarchy->CreateNode("Boss_Bullet", data.icons->GetAnim(MGI_Bio_Bullet), bossBulletSize, {}, characterBulletParent);
+		bullet.node->SetEnabled(false);
+		bossBullets.push_back(bullet);
+	}
 
 	//Create Boss
 	FHParent = data.hierarchy->CreateNode("Final_Hazard_Parent");
@@ -183,18 +354,34 @@ void MinigameFinalBoss::CreateHierarchy(MinigameManagerData data)
 	FHRArmBase->SetRotation(40.0f);
 	FHRWiggle = new Wiggle(0.25f, 30.0f, 50.0f, false);
 	FHRArmBase->components.push_back(FHRWiggle);
-	FHLArm = data.hierarchy->CreateNode("Final_Hazard_Arm_L", data.icons->GetAnim(MGI_Square), { 100.0f, 45.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, FHLArmBase);
+	FHLArm = data.hierarchy->CreateNode("Final_Hazard_Arm_L", data.icons->GetAnim(MGI_Square), { 100.0f, 45.0f }, { 0.0f, 0.0f }, FHLArmBase);
 	FHLArm->SetPosition({ -50.0f, 0.0f });
 	FHLArm->color = { 1.0f, 1.0f, 0.5f, 0.0f };
-	FHRArm = data.hierarchy->CreateNode("Final_Hazard_Arm_R", data.icons->GetAnim(MGI_Square), { 100.0f, 45.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, FHRArmBase);
+	FHRArm = data.hierarchy->CreateNode("Final_Hazard_Arm_R", data.icons->GetAnim(MGI_Square), { 100.0f, 45.0f }, { 0.0f, 0.0f}, FHRArmBase);
 	FHRArm->SetPosition({ 50.0f, 0.0f });
 	FHRArm->color = { 1.0f, 1.0f, 0.5f, 0.0f };
-	FHBody = data.hierarchy->CreateNode("Final_Hazard_Body", data.icons->GetAnim(MGI_Circle), { 300.0f, 100.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, FHParent);
+	FHBody = data.hierarchy->CreateNode("Final_Hazard_Body", data.icons->GetAnim(MGI_Circle), { 300.0f, 100.0f }, { 0.0f, 0.0f }, FHParent);
 	FHBody->SetPosition({ 0.0f, 0.0f });
 	FHBody->color = { 1.0f, 1.0f, 0.5f, 0.0f };
-	FHHead = data.hierarchy->CreateNode("Final_Hazard_Body", data.icons->GetAnim(MGI_Circle), { 75.0f, 100.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, FHParent);
+	FHHead = data.hierarchy->CreateNode("Final_Hazard_Head", data.icons->GetAnim(MGI_Circle), { 75.0f, 100.0f }, { 0.0f, 0.0f }, FHParent);
 	FHHead->SetPosition({ 0.0f, 60.0f });
 	FHHead->color = { 1.0f, 1.0f, 0.5f, 0.0f };
+	FHWeakPoint = data.hierarchy->CreateNode("Final_Hazard_Weak_Point", data.icons->GetAnim(MGI_Circle), { 50.0f,50.0f }, {}, FHParent);
+	FHWeakPoint->SetEnabled(false);
+	FHWeakPoint->color = { 1.0f, 1.0f, 0.0f, 0.0f };
+	data.collision->AddCollision(FHWeakPoint, std::make_shared<CircleCollider>(25.0f));
+
+	//Create Character Bullets
+	characterBulletParent = data.hierarchy->CreateNode("Character_Bullets");
+	characterBullets.clear();
+	for (int i = 0; i < 30; i++)
+	{
+		MinigameBulletData bullet{};
+		bullet.node = data.hierarchy->CreateNode("Character_Bullet", data.icons->GetAnim(MGI_Super_Bullet), characterBulletSize, {}, characterBulletParent);
+		bullet.node->SetEnabled(false);
+		data.collision->AddCollision(bullet.node, std::make_shared<CircleCollider>(10.0f, NJS_POINT3({ 0.0f, 10.0f })));
+		characterBullets.push_back(bullet);
+	}
 
 	//Create Characters
 	characterParent = data.hierarchy->CreateNode("Characters");
@@ -206,4 +393,19 @@ void MinigameFinalBoss::CreateHierarchy(MinigameManagerData data)
 	shadow = data.hierarchy->CreateNode("Shadow", data.icons->GetAnim(MGI_Super_Shadow), { 64.0f, 64.0f }, { 0.0f, 0.0f }, characterParent);
 	shadow->SetPositionGlobal(shadowOffScreenPos);
 	data.collision->AddCollision(shadow, std::make_shared<CircleCollider>(20.0f, NJS_POINT3({ 0.0f, 7.0f})));
+
+	//Create Health Bar
+	FHHealthBarBG = data.hierarchy->CreateNode("Boss_Health_Bar_Background", data.icons->GetAnim(MGI_White_Box), { 200.0f, 10.0f }, { 320.0f, 6.0f });
+	FHHealthBarBG->color = { 1.0f, 1.0f, 0.0f, 0.0f };
+	FHHealthBarBG->SetEnabled(false);
+	FHHealthBar = data.hierarchy->CreateNode("Boss_Health_Bar_Fill", data.icons->GetAnim(MGI_White_Box), { 200.0f, 10.0f }, { 320.0f, 6.0f }, FHHealthBarBG);
+	FHHealthBar->color = { 1.0f, 0.0f, 0.0f, 1.0f };
+
+	//Create Text
+	sonicRings = data.hierarchy->CreateNode("Sonic_Rings", nullptr, { 100.0f, 30.0f }, { 70.0f, 450.0f });
+	sonicRingsText = new TextBox("Sonic\n50", 15.0f, TextAlignment::Left, data.text);
+	sonicRings->renderComponents.push_back(sonicRingsText);
+	shadowRings = data.hierarchy->CreateNode("Shadow_Rings", nullptr, { 100.0f, 30.0f }, { 570.0f, 450.0f });
+	shadowRingsText = new TextBox("Shadow\n50", 15.0f, TextAlignment::Right, data.text);
+	shadowRings->renderComponents.push_back(shadowRingsText);
 }
--- SA2B_Archipelago/SA2B_Archipelago/Items/Minigames/Games/MinigameFinalBoss.h
+++ SA2B_Archipelago/SA2B_Archipelago/Items/Minigames/Games/MinigameFinalBoss.h
@@ -4,6 +4,25 @@
 #include "../Components/TextBox.h"
 #include "../Components/Wiggle.h"
 
+struct MinigameBulletData
+{
+public:
+	SpriteNode* node;
+	NJS_POINT3 velocity;
+};
+
+struct MinigameBulletPattern
+{
+public:
+	int bulletCount;
+	bool pingPong;
+	bool aimAtCharacter;
+	float timeBetween;
+	float angleBetween;
+	float startAngle;
+	float endAngle;
+};
+
 class MinigameFinalBoss : public MinigameBase
 {
 
@@ -35,20 +54,38 @@ private:
 	void RunIntro(MinigameManagerData data);
 	void RunInGame(MinigameManagerData data);
 	void RunSwap(MinigameManagerData data);
+	void CharacterShoot(MinigameManagerData data);
+	void UpdateBullets(MinigameManagerData data);
 	void UpdateCharacterPosition(MinigameManagerData data);
+	void UpdateCharacterRingGrowthDecay(MinigameManagerData data);
+	void UpdateWeakPoint();
+	void UpdateHealthBarFill();
 
 	FinalBossState state;
 	FinalBossIntroState introState;
 
+	float bossMaxHealth = 100.0f;
 	float bossHealth;
+	int sonicRingCount;
+	int shadowRingCount;
 	bool sonicIsActive;
 
-	float characterSpeed = 2.0f;
-	float enemyBulletSpeed = 2.0f;
-	float playerBulletSpeed = 4.0f;
+	float characterSpeed = 4.0f;
+	float enemyBulletSpeed = 3.0f;
+	float characterBulletSpeed = 5.0f;
+	float characterBulletCooldown = 0.25f;
+	float ringDecayCooldown = 2.0f;
+	float ringGrowthCooldown = 1.0f;
+
+	NJS_POINT3 characterBulletSize = { 32.0f, 32.0f };
+	NJS_POINT3 bossBulletSize = { 32.0f, 32.0f };
 
 	NJS_POINT3 sonicOffScreenPos = { 230.0f, 512.0f };
 	NJS_POINT3 shadowOffScreenPos = { 410.0f, 512.0f };
+	
+	float weakPointCooldown = 10.0f;
+	int weakSpotIndex = 0;
+	std::vector<NJS_POINT3> weakPointPositions = { { -120.0f, 17.0f }, { -81.0f, 11.0f }, { -51.0f, 38.0f }, { -14.0f, 25.0f }, { 31.0f, 17.0f }, { 51.0f, 30.0f }, { 80.0f, 38.0f }, { 124.0f, 11.0f } };
 
 	SpriteNode* background;
 	SpriteNode* rightBar;
@@ -70,7 +107,22 @@ private:
 	SpriteNode* FHRArm;
 	SpriteNode* FHBody;
 	SpriteNode* FHHead;
+	SpriteNode* FHWeakPoint;
 	Wiggle* FHLWiggle;
 	Wiggle* FHRWiggle;
+
+	SpriteNode* FHHealthBar;
+	SpriteNode* FHHealthBarBG;
+
+	Timer characterShootTimer;
+	Timer ringGrowthTimer;
+	Timer ringDecayTimer;
+	Timer weakPointTimer;
+	std::vector<Timer> patternTimers = { Timer(), Timer(), Timer(), Timer() };
+
+	SpriteNode* characterBulletParent;
+	std::vector<MinigameBulletData> characterBullets;
+	SpriteNode* bossBulletParent;
+	std::vector<MinigameBulletData> bossBullets;
 
 };
